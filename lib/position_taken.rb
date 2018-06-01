b2ff6951f424ae01a684172319140ed1e3b4257f
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-01 00:16:37 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == ""
-    false
-  elsif board![0]
+  if board[index] == " " || board[index] == "" || board[index] == nil
     false
   end
 end
